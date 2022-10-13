c6e590f275587e332ce62a025f2c3759e6cd31cc
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2022-10-13 18:02:55 -0400

    Time: 452 ms (28.97%), Space: 14.6 MB (77.19%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -5,7 +5,23 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        if target in nums:
-            return nums.index(target)
-        else:
+        # #Fast Solution
+        # if target in nums:
+        #     return nums.index(target)
+        # else:
+        #     return -1
+        
+        
+        # Binary Search Algorithm
+        def binarySearch(nums, target, leftPointer = 0, rightPointer = len(nums)-1):
+            while leftPointer <= rightPointer:
+                midPointer = (leftPointer + rightPointer) // 2
+                if nums[midPointer] == target:
+                    return midPointer
+                elif nums[midPointer] < target:
+                    leftPointer = midPointer + 1
+                else:
+                    rightPointer = midPointer - 1
             return -1
+        
+        return binarySearch(nums, target)
