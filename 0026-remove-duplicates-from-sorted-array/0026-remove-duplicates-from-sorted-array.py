7a8e85768dee3afd245161d23900fa8feed3c52b
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2022-11-25 01:21:00 -0500

    Time: 277 ms (13.34%), Space: 15.7 MB (20.61%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -2,7 +2,7 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         # Sliding Window Technique. Using two pointers, constantly move right pointer
         # and if previous is not the same, update left pointer to current right pointer.
-        # Additionally add onto l to count unique numbers.
+        # Additionally add l each time to l in order to count unique numbers.
         l = 1
         for r in range(1, len(nums)):
             if nums[r] != nums[r - 1]:
