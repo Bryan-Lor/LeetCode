f5405ffec87b8bb6595e6160f10e0a9a232c9d10
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2022-11-25 01:21:09 -0500

    Time: 228 ms (27.39%), Space: 15.5 MB (66.51%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,6 +3,7 @@ class Solution:
         # Sliding Window Technique. Using two pointers, constantly move right pointer
         # and if previous is not the same, update left pointer to current right pointer.
         # Additionally add l each time to l in order to count unique numbers.
+        
         l = 1
         for r in range(1, len(nums)):
             if nums[r] != nums[r - 1]:
