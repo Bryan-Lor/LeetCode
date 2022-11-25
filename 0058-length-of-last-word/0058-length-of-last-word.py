eb5944868bf53983fa1c765c623c87359602b92c
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2022-11-24 23:50:04 -0500

    Time: 50 ms (65.62%), Space: 13.9 MB (80.88%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,5 +1,5 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        # Simply split the string by white space and then get the length of last word
+        # Simply split the string by white space and then get the length of last element/word
         x = s.split()
         return len(x[len(x) - 1])
