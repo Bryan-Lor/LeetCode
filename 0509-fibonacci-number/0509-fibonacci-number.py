c323f0f905a3ac355f6187c9b1356130ab52f152
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2022-11-27 16:25:23 -0500

    Time: 24 ms (80.04%), Space: 13.3 MB (84.66%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+#     # O(n) solution where you iteratively calculate the fibinacci number via two variables.
+#     def fib(self, n):
+#         if n < 2:
+#             return n
+        
+#         n0 = 0
+#         n1 = 1
+#         for i in range(n-1):
+#             fibNum = n0 + n1
+#             n0 = n1
+#             n1 = fibNum
+#         return n1
+    
+    # O(n) solution where you recursively calculate the fibinacci number with memoization
+    def fib(self, n, memo = {}):
+        if n in memo:
+            return memo[n]
+        if n < 2:
+            return n
+        memo[n] = self.fib(n-1, memo) + self.fib(n-2, memo)
+        return memo[n]
