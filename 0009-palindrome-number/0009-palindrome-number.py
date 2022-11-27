9bd7aaa4971fde6289eb570e7d6d45ddccd12874
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2022-11-27 16:53:28 -0500

    Time: 108 ms (68.26%), Space: 13.9 MB (59.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,6 +2,7 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         # O(n) solution where you convert x into a string and compare if it is equal to its
         # reversed sequence. Then simply return its bool value respectively.
+        
         s = str(x)
         if s == s[::-1]:
             return True
