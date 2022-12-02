21f00f47fdd819fa78903c3a0359f2da3158902c
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2022-12-02 18:53:26 -0500

    Time: 87 ms (47.14%), Space: 13.9 MB (31.38%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.py
+++ 0299-bulls-and-cows/0299-bulls-and-cows.py
@@ -1,16 +1,16 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
+        # Brute Force O(n^3) Solution where you create 4 maps to count bulls and cows
         length = len(secret)
         bullsMap = list((guess[i] == secret[i] for i in range(length)))
-        print("BullsMap:", bullsMap)
+        #print("BullsMap:", bullsMap)
         
         # Get secretMap pairs from false values in bulls map via a dictionary comprehension
         secretMap = {secret[i]: secret.count(secret[i]) for i in range(len(secret)) if not bullsMap[i]}
-        print("Secret Map:", secretMap)
+        #print("Secret Map:", secretMap)
         
         # Get cows map from false values in bulls map via a generator
         cowsMap = (index for index in range(len(bullsMap)) if not bullsMap[index])
-        #print("Cows Map:", cowsMap)
         
         bulls = {}
         bullIndex = (index for index in range(len(bullsMap)) if bullsMap[index])
@@ -21,9 +21,6 @@ class Solution:
                 bulls[secret[index]] += 1
         print("Bulls:", bulls)
         
-        # if bulls:
-        #     secretMap = {key: secretMap[key] - bulls[key] for key in secretMap.keys()}
-        #     print("RES:",secretMap)
         for key in bulls:
              if key in secretMap:
                 secretMap[key] -= bulls[key]
@@ -33,9 +30,6 @@ class Solution:
         for index in cowsMap:
             print(guess[index], secret[index])
             if guess[index] in secretMap and secretMap[guess[index]] > 0:
-                print(True, guess[index], index, secretMap[guess[index]])
                 secretMap[guess[index]] -= 1
-                print("Secret Map:", secretMap)
                 cowSum += 1
-        print("Cow Sum:", cowSum)
         return str(sum(bullsMap)) + "A" + str(cowSum) + "B"
