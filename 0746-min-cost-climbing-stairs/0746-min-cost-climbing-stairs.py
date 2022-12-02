c69388a03c7ffd45115d83e10cc6e79d0ffedf84
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2022-12-02 15:25:02 -0500

    Time: 57 ms (96.84%), Space: 13.9 MB (97.59%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -2,6 +2,7 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         # O(n) Solution where you iterate through the list to determine the minimum two steps at a time.
         # Then you store it back into the list (to save memory) and return it at the very end
+        
         n = len(cost)
         for i in range(2, n):
             cost[i] = min(cost[i-1], cost[i-2]) + cost[i]
