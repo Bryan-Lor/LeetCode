2b1907fb973696fffc304996459b1d33607836f0
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2022-12-02 14:42:22 -0500

    Time: 79 ms (9.37%), Space: 13.9 MB (67.13%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -2,8 +2,7 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         # O(n) Solution where you create a set from the string and iterate through that set counting the characters and appending the even values into a list which the sum will be returned
         countList = []
-        setValues = set(s)
-        
+        setValues = set(s)        
         for char in setValues:
             count = s.count(char)
             if count > 1 and count % 2 == 0:
