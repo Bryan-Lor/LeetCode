0c2b5489a5419a5503e92c80f267458d152822fc
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-01-02 05:02:01 -0500

    Time: 50 ms (92.95%), Space: 13.9 MB (40.76%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -2,18 +2,17 @@ class Solution:
     def isOneBitCharacter(self, bits: List[int]) -> bool:
         # O(n) Solution with sliding window technique. Run two pointers
         # throughout the list to determine if two bit or one bit.
-        if len(bits) < 2: return True
+        if len(bits) == 1: return True
         left = 0
         right = 1
         while right < len(bits) - 1:
             if bits[left] == 1:
                 right += 2
-                left = right -1
+                left += 2
                 if (right > len(bits) - 1):
                     right = len(bits) - 1
-                    left = right
             else:
                 right += 1
-                left = right - 1
+                left += 1
                 
         return (bits[left] != 1)
