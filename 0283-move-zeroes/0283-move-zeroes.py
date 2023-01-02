1d348b58145af81515deffefa3e1d3a0d3408f2a
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-01-02 03:50:42 -0500

    Time: 184 ms (73.80%), Space: 15.5 MB (63.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,7 +3,6 @@ class Solution:
         # O(n) solution. Iteratively scan and grab indexes of zeroes into a list.
         # For every zero found, delete it via index and add it back into the
         # end of the list.
-        
         zeroIndexes = []
         for i in range(len(nums)):
             if nums[i] == 0:
