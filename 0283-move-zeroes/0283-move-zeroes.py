8273cf1c533b0d1b003ea666f1ca590bf73ee716
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-01-02 03:44:24 -0500

    Time: 186 ms (72.65%), Space: 15.5 MB (97.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,6 +1,8 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        # O(n) solution. 
+        # O(n) solution. Iteratively scan and delete every zero.
+        # For every zero deleted, additionally add it back into the
+        # end of the list.
         zeroIndexes = []
         for i in range(len(nums)):
             if nums[i] == 0:
@@ -13,3 +15,4 @@ class Solution:
             zeroIndexes[i] = 0
             counter+=1
         nums.extend(zeroIndexes)
+                
