a9ed21789e2f9393938003b14b337a71bc43ff21
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-01-02 02:30:12 -0500

    Time: 30 ms (92.94%), Space: 13.8 MB (56.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -2,6 +2,7 @@ class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         # Checks if the word is all upper, all lower, or capitalized. Then return
         # the respective boolean value.
+        
         if (word == word.upper() or word == word.lower() or word == word.capitalize()):
             return True
         return False
