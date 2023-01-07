b94b9a09d362691383b7bf6632b3d52401157f78
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-01-07 16:17:56 -0500

    Time: 137 ms (87.73%), Space: 16.6 MB (93.54%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # O(n) solution that constructs a dictionary with the amount of times
-        # the element shows, then you simply retrieve the element that shows
-        # only once [O(1) operation].
+        # the element shows. Then you simply retrieve the element that shows
+        # only once in constant time O(1).
         numsMap = Counter(nums)
         return list(numsMap.keys())[list(numsMap.values()).index(1)]
