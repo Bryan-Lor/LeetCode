b2ddeddf52387dd96690c130f319e7f17238615b
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-01-07 16:42:14 -0500

    Time: 46 ms (80.92%), Space: 15.2 MB (15.52%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,5 +1,8 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
+        # O(n) solution that iteratively checks if sequential numbers leading to
+        # n is divisable by 3 and 5. Then append corresponding fizz/buzz/fizzbuzz/i 
+        # value into a list and return it after the loop.
         l = []
         for i in range(1, n+1):
             s = ""
