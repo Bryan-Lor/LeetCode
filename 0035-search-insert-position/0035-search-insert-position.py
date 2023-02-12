e1aed1e4946513f402c10ef1c56e32f3ad5f8119
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-02-11 22:40:54 -0500

    Time: 56 ms (43.16%), Space: 14.7 MB (32.20%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,20 +1,17 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        # O(log n) Solution using binary search to figure out where to place the target number.
         l = 0
         r = len(nums) - 1
         
         while (l <= r):
-            m = (l + r) // 2
-            #print(m, nums[m])
-            print(l, r, m)
-            
+            m = (l + r) // 2            
             if  nums[m] == target:
                 return m
             if nums[m] > target:
                 r = m - 1
             if nums[m] < target:
                 l = m + 1
-                
         if(target > nums[m]):
             return m + 1
         else:
