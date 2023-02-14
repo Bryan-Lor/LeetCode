23eede911be44b0f77bcd83f9d831edc8ea481a2
Bryan Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-02-13 22:30:55 -0500

    Time: 38 ms (42.76%), Space: 13.8 MB (96.89%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
@@ -1,6 +1,6 @@
 class Solution:
     def removeDigit(self, number: str, digit: str) -> str:
-        # O(N^2) Brute Force Solution
+        # O(N^2) Brute Force Solution that creates a new string at each target digit and stores the highest number
         maxNum = ""
         for i in range(len(number)):
             if number[i] == digit:
